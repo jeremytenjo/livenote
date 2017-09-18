08fe56f5618466c3b8ec33e207f99fbe8e7fbabd
Jeremy Tenjo <tenjojeremy@gmail.com>
2017-09-18 17:32:24 -0400

    Update

--- app/src/containers/Directory.js
+++ app/src/containers/Directory.js
@@ -11,53 +11,64 @@ import {connect} from 'react-redux';
 import {Set_RecentNotes, Change_TopBar_Title, Hide_Snackbar, folderSelection} from '../state/actions/index';
 
 function mapStateToProps(state) {
-	return {status: state.RecentNotes}
+  return {status: state.RecentNotes}
 }
 
 function mapDispatchToProps(dispatch) {
-	return bindActionCreators({
-		Set_RecentNotes, Change_TopBar_Title, Hide_Snackbar, folderSelection
-	}, dispatch)
+  return bindActionCreators({
+    Set_RecentNotes,
+    Change_TopBar_Title,
+    Hide_Snackbar,
+    folderSelection
+  }, dispatch)
 }
 class Directory extends React.Component {
 
-	//initial state
-	constructor(props) {
-		super(props)
-		this.state = {
-			data: 'initial'
+  //initial state
+  constructor(props) {
+    super(props)
+    this.state = {
+      data: 'initial'
+    }
+  }
+
+  //Methods
+  componentWillMount() {
+
+    //check if features are available
+		if (window.MediaRecorder === null) {
+			alert('Sorry Audio recording and Voice recognittion are not Supported on your device')
 		}
-	}
 
-	//Methods
-componentWillMount(){
-	this.props.Change_TopBar_Title('Directory');
-	this.props.folderSelection(false);
-}
-	openRecord = () => {
-		this.props.history.push(`/record`);
-	}
-	render() {
-		//Properties
 
-		//Style
-		const FloatingButtonCon = styled.span `
+
+    this.props.Change_TopBar_Title('Directory');
+    this.props.folderSelection(false);
+  }
+  openRecord = () => {
+    this.props.history.push(`/record`);
+  }
+  render() {
+    //Properties
+
+    //Style
+    const FloatingButtonCon = styled.span `
 position: fixed;
 bottom: 20px;
 right: 0;
 `;
-		//Template
-		return (
-			<div>
-				<Recent/>
-				<Folders/>
-				<Notes/>
-				<FloatingButtonCon onClick={this.openRecord}>
-					<FloatingButton/>
-				</FloatingButtonCon>
-			</div>
-		);
-	}
+    //Template
+    return (
+      <div>
+        <Recent/>
+        <Folders/>
+        <Notes/>
+        <FloatingButtonCon onClick={this.openRecord}>
+          <FloatingButton/>
+        </FloatingButtonCon>
+      </div>
+    );
+  }
 
 }
 export default connect(mapStateToProps, mapDispatchToProps)(withRouter(Directory));
