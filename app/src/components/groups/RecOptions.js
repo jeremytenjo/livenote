a2f27f70c5f1031b32d43c9942e9d97a322a2c78
Jeremy Tenjo <tenjojeremy@gmail.com>
2017-09-21 18:30:42 -0400

    Update

--- app/src/components/groups/RecOptions.js
+++ app/src/components/groups/RecOptions.js
@@ -228,7 +228,8 @@ class RecOptions extends React.Component {
 							mountainsRef.putString(d.image, 'data_url').then((snapshot) => {
 								// console.log(snapshot.metadata.downloadURLs[0]);
 								//write to database
-								firebase.database().ref(`users/${firebase.auth().currentUser.uid}/notes`).push({
+								// firebase.database().ref(`users/${firebase.auth().currentUser.uid}/notes`).push({
+								firebase.database().ref(`users/${firebase.auth().currentUser.uid}/masterNotes/notes/${key}`).push({
 									masterNote_id: key,
 									name: this.props.noteName,
 									title: d.title,
