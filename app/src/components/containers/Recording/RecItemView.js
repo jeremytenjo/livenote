c908f574669a6fed2a8163bd3799ecdeb6230382
Jeremy Tenjo <tenjojeremy@gmail.com>
2017-10-23 01:44:31 -0400

    Update

--- app/src/components/containers/Recording/RecItemView.js
+++ app/src/components/containers/Recording/RecItemView.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components'
+import Loadable from 'react-loadable';
 //State
 
 import {bindActionCreators} from 'redux';
@@ -7,6 +8,18 @@ import {connect} from 'react-redux';
 import {NotePreview_Show, NotePreview_Set} from '../../../state/actions/index';
 import NoteItem from '../../global/NoteItem.js';
 
+// const ItemOnlyText = Loadable({
+// 	loader: () => import ('../../global/Item_OnlyText.js'),
+// 	loading: () => null
+// });
+// const ItemOnlyImage = Loadable({
+// 	loader: () => import ('../../global/Item_OnlyImage.js'),
+// 	loading: () => null
+// });
+// const ItemTextImage = Loadable({
+// 	loader: () => import ('../../global/Item_TextImage.js'),
+// 	loading: () => null
+// });
 
 //Set global state to prop
 function mapStateToProps(state) {
@@ -54,12 +67,28 @@ class RecItemView extends React.Component {
     itemList = '';
     itemList = items.map((item, i) => {
       // console.log(item);
-      itemList = <span key={i} data-time={item.time} data-title={item.title} data-image={item.imageUrl} data-desc={item.desc} onClick={this.showPreview}>
-        <NoteItem time={item.time} title={item.title} desc={item.desc} image={item.imageUrl}/>
+      itemList = <span key={i} data-time={item.time} data-title={item.title} data-image={item.image} data-desc={item.desc} onClick={this.showPreview}>
+        <NoteItem time={item.time} title={item.title} desc={item.desc} image={item.image}/>
       </span>
 
       return itemList
 
+      // if (item.desc && item.image !== '') {
+      	// list = <span data-time={item.time} data-title={item.title} data-image={item.image} data-desc={item.desc} onClick={this.showPreview} key={i}>
+				// 	<ItemTextImage time={item.time} title={item.title} desc={item.desc} image={item.image}/></span>;
+      	// return list
+      //
+      // } else if (item.desc === '') {
+      // 	list = <span data-time={item.time} data-title={item.title} data-image={item.image} onClick={this.showPreview} key={i}><ItemOnlyImage time={item.time} title={item.title} image={item.image}/></span>;
+      // 	return list
+      //
+      // } else if (item.image === '') {
+      // 	list = <span data-time={item.time} data-title={item.title} data-desc={item.desc} onClick={this.showPreview} key={i}><ItemOnlyText time={item.time} title={item.title} desc={item.desc}/></span>;
+      // 	return list
+      //
+      // }
+
+      // return itemList
     });
 
     //delay so that has time to scroll to end
