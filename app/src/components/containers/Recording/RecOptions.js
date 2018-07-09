7bf371b00c1fa9137e679ad5045c2a0824317a38
tenjojeremy <tenjojeremy@gmail.com>
2018-07-08 17:01:11 -0700

    Update

--- app/src/components/containers/Recording/RecOptions.js
+++ app/src/components/containers/Recording/RecOptions.js
@@ -5,6 +5,7 @@ import Note_icon from '../../../images/icons/Note.svg'
 import Camera_icon from '../../../images/icons/Camara.svg'
 import Stop_icon from '../../../images/icons/Stop.svg'
 import Pause_icon from '../../../images/icons/Pause.svg'
+import backgroundColor_icon from '../../../images/icons/mediaNotification.png'
 import Play_icon from '../../../images/icons/Play.svg'
 import firebase from 'firebase'
 import ImageCompressor from '@xkeshi/image-compressor'
@@ -48,7 +49,8 @@ function mapStateToProps(state) {
     noteName: state.Note_Name,
     FolderSelectionName: state.FolderSelection_Name,
     MasterNote_ID: state.MasterNote_ID,
-    FolderSelection_ID_Name: state.FolderSelection_ID
+    FolderSelection_ID_Name: state.FolderSelection_ID,
+    TopBar_Title: state.TopBar_Title
   }
 }
 //define actions
@@ -124,6 +126,18 @@ class RecOptions extends React.Component {
   }
 
   recControl = (stream) => {
+    //Notification controls
+    let MediaMetadata = window.MediaMetadata || function() {}
+
+    if ('mediaSession' in navigator) {
+      navigator.mediaSession.metadata = new MediaMetadata({
+        title: this.props.TopBar_Title,
+        artwork: [{ src: backgroundColor_icon }]
+      })
+      navigator.mediaSession.setActionHandler('play', () => this.play())
+      navigator.mediaSession.setActionHandler('pause', () => this.pause())
+    }
+
     var recorder = new MediaRecorder(stream)
 
     const SpeechRecognition =
